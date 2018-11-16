60fcb52471c3cad6a9d0c183ad0e40eea1e1e3fb
Taylor O'Reilly <Taylor@TaylorOReilly.com>
2018-11-16 14:02:29 -0500

    update schema

--- graphql/PostSchema.js
+++ graphql/PostSchema.js
@@ -1,31 +1,36 @@
 import Post from "../models/Post"
+import { UserTC } from './UserSchema'
 import { composeWithMongoose } from 'graphql-compose-mongoose';
-
+import { TypeComposer } from 'graphql-compose';
 let customizationOptions = {}
-const PostTC = composeWithMongoose(Post, customizationOptions);
+export const PostTC = composeWithMongoose(Post, customizationOptions);
 
-
+PostTC.addRelation('author', {
+  resolver: () => UserTC.getResolver('findById'),
+  prepareArgs: { // resolver `findByIds` has `_ids` arg, let provide value to it
+    id: (source) => source.author,
+  },
+  projection: { author: true }
+})
 
 export const postQueries = {
   postById: PostTC.getResolver('findById'),
   postsById: PostTC.getResolver('findByIds'),
-  postOne: PostTC.getResolver('findOne'),
-  postMany: PostTC.getResolver('findMany'),
-  postCount: PostTC.getResolver('count'),
-  postConnection: PostTC.getResolver('connection'),
-  postPagination: PostTC.getResolver('pagination')
+  // postOne: PostTC.getResolver('findOne'),
+  // postMany: PostTC.getResolver('findMany'),
+  // postCount: PostTC.getResolver('count'),
+  // postConnection: PostTC.getResolver('connection'),
+  // postPagination: PostTC.getResolver('pagination')
 }
 
 export const postMutations = {
   postCreateOne: PostTC.getResolver('createOne'),
-  postCreateMany: PostTC.getResolver('createMany'),
+  // postCreateMany: PostTC.getResolver('createMany'),
   postUpdateById: PostTC.getResolver('updateById'),
-  postUpdateOne: PostTC.getResolver('updateOne'),
-  postUpdateMany: PostTC.getResolver('updateMany'),
+  // postUpdateOne: PostTC.getResolver('updateOne'),
+  // postUpdateMany: PostTC.getResolver('updateMany'),
   postRemoveById: PostTC.getResolver('removeById'),
-  postRemoveOne: PostTC.getResolver('removeOne'),
+  // postRemoveOne: PostTC.getResolver('removeOne'),
   postRemoveMany: PostTC.getResolver('removeMany'),
 }
 
-
-export default { PostTC, postQueries, postMutations }

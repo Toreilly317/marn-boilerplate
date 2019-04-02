e1b2ebf9fa5cb9e89a11b33eddc9d265aca34457
Taylor O'Reilly <toreilly317@gmail.com>
2019-04-01 23:43:54 -0400

    update styles

---
+++ CMS/components/common/Form/TextArea.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const TextArea = styled.textarea`
+  font-size: 1rem;
+  padding: 10px;
+`;
+
+export default TextArea;
--- CMS/components/common/PageTitle.js
+++ CMS/components/common/PageTitle.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-const PageTitle = styled.div``;
+const PageTitle = styled.div`
+  display: inline-flex;
+  align-items: center;
+  font-size: 3rem;
+  padding: 1rem;
+  justify-self: flex-start;
+  color: white;
+`;
 
-export default ({ title }) => <PageTitle>{title}</PageTitle>;
+export default PageTitle;

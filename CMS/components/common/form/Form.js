461515f101a270b8977718985ee472d355fc79e4
Taylor O'Reilly <toreilly317@gmail.com>
2019-04-01 23:44:52 -0400

    update styles

--- CMS/components/common/form/Form.js
+++ CMS/components/common/form/Form.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 const Form = styled.form`
-  background: black;
-  padding: 1rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 4rem;
 `;
 
 export default Form;

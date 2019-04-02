011703a26632c280affc9abea1f3761e66773d54
Taylor O'Reilly <toreilly317@gmail.com>
2019-04-01 23:45:30 -0400

    add common components to index

--- CMS/components/common/index.js
+++ CMS/components/common/index.js
@@ -1,12 +1,23 @@
 import Button from './Button';
+import Col from './Col';
 import FileInput from './Form/FileInput';
 import Form from './Form/Form';
 import FormGroup from './Form/FormGroup';
 import Input from './Form/Input';
 import Row from './Row';
 import TagInput from './Form/TagInput';
-import Col from './Col';
+import TextArea from './Form/TextArea';
+import PageTitle from './PageTitle';
 
 export {
- FileInput, Form, FormGroup, Input, Button, Row, Col, TagInput 
+  FileInput,
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Row,
+  Col,
+  TagInput,
+  TextArea,
+  PageTitle,
 };
